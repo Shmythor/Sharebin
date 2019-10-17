6d48e4a87a17e63bd24a61fa6d973cb784ad5f5c
misha <homakhaker@gmail.com>
2019-10-17 17:55:14 +0200

    Added key/value existing check

--- backend/common/models/metadata.js
+++ backend/common/models/metadata.js
@@ -8,8 +8,11 @@ module.exports = function (Metadata) {
     if (ctx.instance) {
       Document.findById(ctx.instance.documentId, function (err, instance) {
         if (instance) {
-          console.log('Metadata was created ' + ctx.instance.value);
-          next();
+          if (instance.metadatas.filter(metadata => metadata.key === ctx.instance.key && metadata.value === ctx.instance.value).length === 0) {
+            console.log('Metadata was created ' + ctx.instance.value);
+            next();
+          }
+          console.log('Metadata with that key and value already exists ' + ctx.instance.value + " : " + ctx.instance.key);
         } else {
           next(new Error('Cant find the document with id: ' + ctx.instance.documentId));
         }
