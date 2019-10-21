06e9d4d32892f2ad10ff62e822c9ba374eb0e139
crigasro <gasparrozalen@gmail.com>
2019-10-21 22:12:26 +0200

    Minor change

--- backend/common/models/client.js
+++ backend/common/models/client.js
@@ -4,7 +4,7 @@ let App = require('../../server/server');
 module.exports = function(Client) {
 
     Client.observe('before save', function createClientStorage(user, next) {
-        let FolderModel = app.models.Folder;
+        let FolderModel = App.models.Folder;
         if (user.isNewInstance)
             FolderModel.createContainer({name: user.email, path: user.email}, (err, folder) => {});
         
