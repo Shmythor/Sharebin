e38f9f39dbf4684cd6dda30528e371ce957e4dd6
misha <homakhaker@gmail.com>
2019-11-05 18:43:52 +0100

    Added the creation of a random url

--- backend/common/models/document.js
+++ backend/common/models/document.js
@@ -1,50 +1,75 @@
 'use strict';
 
 let App = require('../../server/server');
-module.exports = function(Document) {
-    /*Document.sharedClass.methods().forEach(function(method) {
-        console.log(method.name);
-    });*/
-    
-    // clientId mantatory
-    Document.validatesPresenceOf('clientId');
+const crypto = require('crypto');
 
-    Document.download = function(req, res, documentId, cb) {
-        let Folder = App.models.Folder;
-        let Client = App.models.Client;
-        let documentObject;
-        
-        Document.findById(documentId)
-            .then((data) => {
-                if (!data)
-                    throw new Error(`Document with id ${documentId} not found`);
-                
-                console.log(data);
-                documentObject = data;
-                return Client.findById(documentObject.clientId);
-            })
-            .then((clientObject) => {
-                if (!clientObject)
-                    throw new Error(`The user associated with the document with id ${documentId} doesn't exist`);
-                
-                return Folder.download(clientObject.email, documentObject.path, req, res);
-            })
-            .then((file) => cb(null, file))
-            .catch(err => cb(err));      
-    }
+module.exports = function (Document) {
+  /*Document.sharedClass.methods().forEach(function(method) {
+      console.log(method.name);
+  });*/
 
-    Document.remoteMethod('download', {
-        description: "Downloads the document with the specified Id",
-        accepts: [
-            {arg: 'req', type: 'object', http: {source: 'req'}},
-            {arg: 'res', type: 'object', http: {source: 'res'}},
-            {arg: 'documentId', type: 'string'}
-        ],
-        returns: [
-            { arg: 'body', type: 'file', root: true },
-            { arg: 'Content-Type', type: 'string', http: { target: 'header' } }
-        ],
-        http: {verb: 'GET', path: '/:documentId/download'}
-    });
+  // clientId mantatory
+  Document.validatesPresenceOf('clientId');
+
+  Document.download = function (req, res, documentId, cb) {
+    let Folder = App.models.Folder;
+    let Client = App.models.Client;
+    let documentObject;
+
+    Document.findById(documentId)
+      .then((data) => {
+        if (!data)
+          throw new Error(`Document with id ${documentId} not found`);
+
+        console.log(data);
+        documentObject = data;
+        return Client.findById(documentObject.clientId);
+      })
+      .then((clientObject) => {
+        if (!clientObject)
+          throw new Error(`The user associated with the document with id ${documentId} doesn't exist`);
+
+        return Folder.download(clientObject.email, documentObject.path, req, res);
+      })
+      .then((file) => cb(null, file))
+      .catch(err => cb(err));
+  }
+
+  Document.createURL = function (req, res, documentId, cb) {
+    Document.findById(documentId).then((data) => {
+      if (data.urlToShare !== null) {
+        return crypto.randomBytes(20).toString('hex');
+      }
+      return data.urlToShare;
+    }).then((url) => cb(null, url))
+      .catch(err => cb(err));
+  }
+
+  Document.remoteMethod('download', {
+    description: "Downloads the document with the specified Id",
+    accepts: [
+      {arg: 'req', type: 'object', http: {source: 'req'}},
+      {arg: 'res', type: 'object', http: {source: 'res'}},
+      {arg: 'documentId', type: 'string'}
+    ],
+    returns: [
+      {arg: 'body', type: 'file', root: true},
+      {arg: 'Content-Type', type: 'string', http: {target: 'header'}}
+    ],
+    http: {verb: 'GET', path: '/:documentId/download'}
+  });
+
+  Document.remoteMethod('createURL', {
+    description: "Create a random url to share",
+    accepts: [
+      {arg: 'req', type: 'object', http: {source: 'req'}},
+      {arg: 'res', type: 'object', http: {source: 'res'}},
+      {arg: 'documentId', type: 'string'}
+    ],
+    returns: [
+      {arg: 'url', type: 'string', root: true}
+    ],
+    http: {verb: 'GET', path: '/createURL'}
+  });
 
 };
