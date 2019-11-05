8a1ac4b33534385540d99efc42c0fc7bfad519b5
misha <homakhaker@gmail.com>
2019-11-05 17:49:58 +0100

    Changed metadata log

--- backend/common/models/metadata.js
+++ backend/common/models/metadata.js
@@ -1,22 +1,28 @@
 'use strict';
 
 const app = require('../../server/server');
-module.exports = function(Metadata) {
-    /*Metadata.sharedClass.methods().forEach(function(method) {
-        console.log(method.name);
-    });*/
+module.exports = function (Metadata) {
+  /*Metadata.sharedClass.methods().forEach(function(method) {
+      console.log(method.name);
+  });*/
 
-    Metadata.observe('before save', function checkDocumentExists(ctx, next) {
-        const Document = app.models.Document;
-        if (ctx.instance) {
-            Document.findById(ctx.instance.documentId, function(err, instance) {
-                if (instance) {
-                    console.log('Metadata was created ' + ctx.instance.value);
-                    next();
-                } else {
-                    next(new Error('Cant find the document with id: ' + ctx.instance.documentId));
-                }
-            });
+  Metadata.observe('before save', function checkDocumentExists(ctx, next) {
+    const Document = app.models.Document;
+    if (ctx.instance) {
+      Document.findById(ctx.instance.documentId, function (err, instance) {
+        if (instance) {
+          console.log(instance.metadatas);
+          /*if (instance.metadatas.filter(metadata => metadata.key === ctx.instance.key && metadata.value === ctx.instance.value).length > 0) {
+            next(new Error('Metadata with that value already exists for that document'));
+          }*/
+          console.log(`Metadata was created with value: ${ctx.instance.value} 
+                       key: ${ctx.instance.key} 
+                       for document: ${ctx.instance.documentId}`);
+          next();
+        } else {
+          next(new Error('Cant find the document with id: ' + ctx.instance.documentId));
         }
-    });
+      });
+    }
+  });
 };
