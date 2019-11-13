e68e352e8a08429025831ed38ccded71b9b313ee
crigasro <gasparrozalen@gmail.com>
2019-11-13 18:33:30 +0100

    Size limit test done

--- backend/tests/size-test.js
+++ backend/tests/size-test.js
@@ -1,30 +1,59 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const expect = require('chai').expect;
+const fs = require('fs');
+const path = require('path');
+const FormData = require('form-data');
+
 
 // "root": "/home/crigasro/Escritorio/Sharebin/backend/server/storage",
 
 chai.use(chaiHttp);
 const url= 'http://localhost:3000';
 
-let testClient = { 
-    "username": 'Test',
-    "email": 'test1@doe.com', 
-    "password": 'password', 
-    "name": 'Test', 
-    "createDate": Date.now() 
-};
+// ID of the Test client (already registered)
+let testClientID = "5dcc3b6e68cdd20eb4017d97";
 
-describe('Insert a client: ',()=>{
+// Este test funciona, pero para no marear lo comento
+// Además no entraría para estos test
+// describe('Insert a client: ',()=>{
 
-	it('should insert a client', (done) => {
-		chai.request(url)
-			.post('/api/Clients')
-			.send(testClient)
-			.end((err,res) => {
-				console.log(res.body)
+// 	it('should insert a client', (done) => {
+// 		chai.request(url)
+// 			.post('/api/Clients')
+// 			.send(testClient)
+// 			.end((err, res) => {
+//                 console.log(res.body)
+//                 testClientID = res.body.id;
+//                 console.log(testClientID)
+// 				expect(res).to.have.status(200);
+// 				done();
+// 			});
+// 	});
+// });
+
+describe("File size limit test", () => {
+    let direction = path.join(__dirname, '/git_guia.pdf');   
+    let file = fs.readFileSync(direction);
+    let description = "Archivo test de subida";
+
+    let formData = new FormData();
+    formData.append('file', file, file.name);
+    formData.append('description', description);
+    
+    it("should POST a file under 20MB", () => {
+        chai.request(url)
+            .post(`/api/Clients/${testClientID}/uploadDocument`)
+            .set('content-type', 'application/x-www-form-urlencoded')
+            // .send({myparam: 'test'})
+            .send(formData)
+            .end((err, res) => {
+                if(err) {
+                    console.log('An error ocurred: ', err)
+                }
+                // console.log(res.body)
 				expect(res).to.have.status(200);
 				done();
-			});
-	});
-});
+            })
+    })
+})
