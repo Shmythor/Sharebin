44f70e0b8cd61ee530b7093defa56eb2798bdee0
Johans2016 <johansmulen@yahoo.com>
2019-11-19 03:41:10 +0100

    mejorando subida

--- frontend/src/app/pages/home/components/ventanaemerg/ventanaemerg.component.ts
+++ frontend/src/app/pages/home/components/ventanaemerg/ventanaemerg.component.ts
@@ -102,7 +102,6 @@ export class VentanaemergComponent implements OnInit {
       console.log('Error al subir documento: ' + err);
     });
   }
-
   addDataTable(data: File) {
     document.getElementById('fileNameTable').innerHTML = '<strong>' + data.name + '</strong>';
     document.getElementById('fileSizeTable').innerHTML = '' + data.size + ' Bytes';
@@ -128,6 +127,7 @@ export class VentanaemergComponent implements OnInit {
       document.getElementById('fileUploadLimit').style.display = 'none';
     }, 2000);
   }
+
 }
 
 
