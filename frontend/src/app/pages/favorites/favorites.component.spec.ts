e1a7463a31634d194ed143533fe909292c05fb14
misha <homakhaker@gmail.com>
2019-12-10 20:50:52 +0100

    Working on the test

--- frontend/src/app/pages/favorites/favorites.component.spec.ts
+++ frontend/src/app/pages/favorites/favorites.component.spec.ts
@@ -1,6 +1,15 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { FavoritesComponent } from './favorites.component';
+import {FavoritesComponent} from './favorites.component';
+import {CUSTOM_ELEMENTS_SCHEMA, InjectionToken} from "@angular/core";
+import {ClientApi, DocumentApi, SDKModels} from "../../services/lb-api/services/custom";
+import {HttpClient, HttpHandler} from "@angular/common/http";
+import {SocketConnection} from "../../services/lb-api/sockets/socket.connections";
+import {SocketDriver} from "../../services/lb-api/sockets/socket.driver";
+import {LoopBackAuth} from "../../services/lb-api/services/core";
+import {InternalStorage} from "../../services/lb-api";
+import {MatDialog} from "@angular/material/dialog";
+import {Overlay} from "@angular/cdk/overlay";
 
 describe('FavoritesComponent', () => {
   let component: FavoritesComponent;
@@ -8,9 +17,25 @@ describe('FavoritesComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ FavoritesComponent ]
+      declarations: [
+        FavoritesComponent
+      ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      providers: [
+        ClientApi,
+        HttpClient,
+        HttpHandler,
+        SocketConnection,
+        SocketDriver,
+        SDKModels,
+        LoopBackAuth,
+        InternalStorage,
+        DocumentApi,
+        MatDialog,
+        Overlay
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
@@ -21,5 +46,10 @@ describe('FavoritesComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should only be favorites documents', () => {
+    //const count = component.dataFiltered.filter(doc => doc.isFavourite);
+    expect(component).toBeUndefined();
   });
 });
