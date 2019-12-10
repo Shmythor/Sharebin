fd980d2e04783c9a16b9561c5c6aebce64ae5090
Juan Carlos <juancarlosjaen.jaeniglesias@gmail.com>
2019-12-10 13:32:36 +0100

    Arreglado bugs de papelera

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,9 +1,6 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {MaterialModule} from './material/material.module';
-import {MatTabsModule} from '@angular/material';
-import {MatChipsModule} from '@angular/material/chips';
-import {MatIconModule} from '@angular/material/icon';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { NgxDropzoneModule } from 'ngx-dropzone';
 
