0b4f3dce6b97da38721f8904688ef995daaab47b
Johans2016 <johansmulen@yahoo.com>
2019-12-10 19:08:17 +0100

    subir desde el drop-zone

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -41,6 +41,7 @@ import {SearchbarComponent} from './shared/components/searchbar/searchbar.compon
 import { ThemeItemComponent } from './pages/themes/components/theme-item/theme-item.component';
 
 import { DatePipe } from '@angular/common';
+import { MatTabsModule, MatChipsModule, MatIconModule, MatToolbarModule, MatCardModule } from '@angular/material';
 
 
 
@@ -76,7 +77,12 @@ import { DatePipe } from '@angular/common';
     ModalModule,
     GridModule,
     NgbModule,
-    NgxDropzoneModule
+    NgxDropzoneModule,
+    MatTabsModule,
+    MatChipsModule,
+    MatIconModule,
+    MatCardModule,
+    MatToolbarModule
   ],
   providers: [
     DatePipe,
