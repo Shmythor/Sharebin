4e8126870f7c0b9f65ced7600038ead0cceb370f
misha <homakhaker@gmail.com>
2019-12-12 16:15:40 +0100

    FAvorite test

--- frontend/src/app/pages/favorites/favorites.component.spec.ts
+++ frontend/src/app/pages/favorites/favorites.component.spec.ts
@@ -1,26 +1,39 @@
-import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import {async, ComponentFixture, getTestBed, TestBed} from '@angular/core/testing';
 
 import {FavoritesComponent} from './favorites.component';
-import {CUSTOM_ELEMENTS_SCHEMA, InjectionToken} from "@angular/core";
-import {ClientApi, DocumentApi, SDKModels} from "../../services/lb-api/services/custom";
+import {CUSTOM_ELEMENTS_SCHEMA, InjectionToken, NO_ERRORS_SCHEMA} from "@angular/core";
+import {AuditorApi, ClientApi, DocumentApi, MetadataApi, SDKModels} from "../../services/lb-api/services/custom";
 import {HttpClient, HttpHandler} from "@angular/common/http";
 import {SocketConnection} from "../../services/lb-api/sockets/socket.connections";
 import {SocketDriver} from "../../services/lb-api/sockets/socket.driver";
 import {LoopBackAuth} from "../../services/lb-api/services/core";
 import {InternalStorage} from "../../services/lb-api";
-import {MatDialog} from "@angular/material/dialog";
+import {MatDialog, MatDialogModule} from "@angular/material/dialog";
 import {Overlay} from "@angular/cdk/overlay";
+import {CommonModule, DatePipe} from "@angular/common";
+import {ScrollDispatchModule} from "@angular/cdk/scrolling";
+import {By} from "@angular/platform-browser";
+import {timeout} from "rxjs/operators";
 
 describe('FavoritesComponent', () => {
   let component: FavoritesComponent;
   let fixture: ComponentFixture<FavoritesComponent>;
+  let docApi : DocumentApi;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        CommonModule,
+        ScrollDispatchModule,
+        MatDialogModule
+      ],
       declarations: [
         FavoritesComponent
       ],
-      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      schemas: [
+        CUSTOM_ELEMENTS_SCHEMA,
+        NO_ERRORS_SCHEMA
+      ],
       providers: [
         ClientApi,
         HttpClient,
@@ -32,24 +45,34 @@ describe('FavoritesComponent', () => {
         InternalStorage,
         DocumentApi,
         MatDialog,
-        Overlay
+        Overlay,
+        MetadataApi,
+        AuditorApi,
+        DatePipe
       ]
-    })
-      .compileComponents();
+    }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(FavoritesComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should only be favorites documents', () => {
-    //const count = component.dataFiltered.filter(doc => doc.isFavourite);
-    expect(component).toBeUndefined();
+  it('should only show favorites documents', () => {
+    /*const docApi = fixture.debugElement.injector.get(DocumentApi);
+    let data, dataFiltered;
+    const filter = {
+      where: {clientId: '5da5ecd69b976d2bcac8298c', isDeleted: false},
+      include: 'metadatas',
+    };*/
+    console.log('QUIERO LLORAR');
+    component.getUserItemList();
+    console.log(component.dataFiltered);
+
   });
+
 });
